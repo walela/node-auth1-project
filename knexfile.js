c764901c7dd37afb91bcf83704ae48a5efb94f3a
Austin Walela <austinmasibo@gmail.com>
2020-03-25 17:00:33 +0300

    added migration and created db

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './data/users.sqlite3'
+      filename: './database/users.sqlite3'
     },
     pool: {
       afterCreate: (conn, done) => {
